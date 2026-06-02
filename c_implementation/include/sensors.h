fd57fcc12148b82d2e122931351ef9499cb39307
thomasduroy <thomas.duroy72@gmail.com>
2026-06-02 10:12:54 +0200

    refactor sensors

--- c_implementation/include/sensors.h
+++ c_implementation/include/sensors.h
@@ -5,18 +5,26 @@
 #include "utils.h"
 
 typedef struct {
+    double acc_meas[3];
+    double gyro_meas[3];
+    double mag_meas[3];
+    double mocap_meas[3];
     double p_buffer[2][3]; 
     int write_index;
 } SensorsState;
 
 void sensors_init(SensorsState* state, Param* param);
 
-void sensors_accelerometer(double* X_dot, double* X, double* acc_meas, Param* param);
+void accelerometer_outputs(double* acc_meas, Param* param, SensorsState* state);
+void accelerometer_update(double* X_dot, double* X, Param* param, SensorsState* state);
 
-void sensors_gyrometer(double* X, double* gyro_meas, Param* param);
+void gyrometer_outputs(double* gyro_meas, Param* param, SensorsState* state);
+void gyrometer_update(double* X, Param* param, SensorsState* state);
 
-void sensors_magnetometer(double* X, double* mag_meas, Param* param);
+void magnetometer_outputs(double* mag_meas, Param* param, SensorsState* state);
+void magnetometer_update(double* X, Param* param, SensorsState* state);
 
-void sensors_mocap(double* X, double* mocap_meas, SensorsState* state, Param* param);
+void mocap_outputs(double* mocap_meas, Param* param, SensorsState* state);
+void mocap_update(double* X, double* mocap_meas, Param* param, SensorsState* state);
 
 #endif // SENSORS_H
--- c_implementation/src/sensors.c
+++ c_implementation/src/sensors.c
@@ -2,15 +2,21 @@
 #include <math.h>
 
 void sensors_init(SensorsState* state, Param* param) {
+    memset(state->acc_meas, 0, sizeof(state->acc_meas));
+    memset(state->gyro_meas, 0, sizeof(state->gyro_meas));
+    memset(state->mag_meas, 0, sizeof(state->mag_meas));
+    memset(state->mocap_meas, 0, sizeof(state->mocap_meas));
     state->write_index = 0;
-    for (int i = 0; i < 2; i++) {
-        state->p_buffer[i][0] = 0.0;
-        state->p_buffer[i][1] = 0.0;
-        state->p_buffer[i][2] = 0.0;
+    memset(state->p_buffer, 0, sizeof(state->p_buffer));
+}
+
+void accelerometer_outputs(double* acc_meas, Param* param, SensorsState* state) {
+    for (int i = 0; i < 3; i++) {
+        acc_meas[i] = state->acc_meas[i];
     }
 }
 
-void sensors_accelerometer(double* X_dot, double* X, double* acc_meas, Param* param) {
+void accelerometer_update(double* X_dot, double* X, Param* param, SensorsState* state) {
     double phi = X[3];   double theta = X[4]; double psi = X[5];
 
     double cphi = cos(phi);   double sphi = sin(phi);
@@ -24,22 +30,33 @@ void sensors_accelerometer(double* X_dot, double* X, double* acc_meas, Param* pa
     };
 
     for (int i = 0; i < 3; i++) {
-        acc_meas[i] = R_B[0][i] * X_dot[6] 
+        state->acc_meas[i] = R_B[0][i] * X_dot[6] 
                     + R_B[1][i] * X_dot[7] 
                     + R_B[2][i] * (X_dot[8] + param->g);
                     
-        acc_meas[i] += param->b_acc + generate_gaussian_noise(param->sigma_acc, param->f_acc);
+        state->acc_meas[i] += param->b_acc + generate_gaussian_noise(param->sigma_acc, param->f_acc);
     }
 }
 
-
-void sensors_gyrometer(double* X, double* gyro_meas, Param* param) {
-    gyro_meas[0] = X[9]  + param->b_gyro + generate_gaussian_noise(param->sigma_omega, param->f_gyro);
-    gyro_meas[1] = X[10] + param->b_gyro + generate_gaussian_noise(param->sigma_omega, param->f_gyro);
-    gyro_meas[2] = X[11] + param->b_gyro + generate_gaussian_noise(param->sigma_omega, param->f_gyro);
+void gyrometer_outputs(double* gyro_meas, Param* param, SensorsState* state) {
+    for (int i = 0; i < 3; i++) {
+        gyro_meas[i] = state->gyro_meas[i];
+    }
 }
 
-void sensors_magnetometer(double* X, double* mag_meas, Param* param) {
+void gyrometer_update(double* X, Param* param, SensorsState* state) {
+    state->gyro_meas[0] = X[9]  + param->b_gyro + generate_gaussian_noise(param->sigma_omega, param->f_gyro);
+    state->gyro_meas[1] = X[10] + param->b_gyro + generate_gaussian_noise(param->sigma_omega, param->f_gyro);
+    state->gyro_meas[2] = X[11] + param->b_gyro + generate_gaussian_noise(param->sigma_omega, param->f_gyro);
+}
+
+void magnetometer_outputs(double* mag_meas, Param* param, SensorsState* state) {
+    for (int i = 0; i < 3; i++) {
+        mag_meas[i] = state->mag_meas[i];
+    }
+}
+
+void magnetometer_update(double* X, Param* param, SensorsState* state) {
     double phi = X[3];   double theta = X[4]; double psi = X[5];
 
     double cphi = cos(phi);   double sphi = sin(phi);
@@ -53,28 +70,34 @@ void sensors_magnetometer(double* X, double* mag_meas, Param* param) {
     };
 
     for (int i = 0; i < 3; i++) {
-        mag_meas[i] = R_B[0][i] * param->m_N + R_B[1][i] * param->m_E + R_B[2][i] * param->m_D;
+        state->mag_meas[i] = R_B[0][i] * param->m_N + R_B[1][i] * param->m_E + R_B[2][i] * param->m_D;
     }
 
-    double mx = mag_meas[0]; double my = mag_meas[1]; double mz = mag_meas[2];
+    double mx = state->mag_meas[0]; double my = state->mag_meas[1]; double mz = state->mag_meas[2];
     
     for (int i = 0; i < 3; i++) {
-        mag_meas[i] = param->A_mag[i][0] * mx + param->A_mag[i][1] * my + param->A_mag[i][2] * mz
+        state->mag_meas[i] = param->A_mag[i][0] * mx + param->A_mag[i][1] * my + param->A_mag[i][2] * mz
                     + param->B_mag[i]
                     + generate_gaussian_noise(param->sigma_mag, param->f_mag);
     }
 }
 
-void sensors_mocap(double* X, double* mocap_meas, SensorsState* state, Param* param) {
+void mocap_outputs(double* mocap_meas, Param* param, SensorsState* state) {
+    for (int i = 0; i < 3; i++) {
+        mocap_meas[i] = state->mocap_meas[i];
+    }
+}
+
+void mocap_update(double* X, double* mocap_meas, Param* param, SensorsState* state) {
     state->p_buffer[state->write_index][0] = X[0];
     state->p_buffer[state->write_index][1] = X[1];
     state->p_buffer[state->write_index][2] = X[2];
 
     int read_index = (state->write_index + 1) % 2;
 
-    mocap_meas[0] = state->p_buffer[read_index][0] + generate_gaussian_noise(param->sigma_pos, param->f_MoCap);
-    mocap_meas[1] = state->p_buffer[read_index][1] + generate_gaussian_noise(param->sigma_pos, param->f_MoCap);
-    mocap_meas[2] = state->p_buffer[read_index][2] + generate_gaussian_noise(param->sigma_pos, param->f_MoCap);
+    state->mocap_meas[0] = state->p_buffer[read_index][0] + generate_gaussian_noise(param->sigma_pos, param->f_MoCap);
+    state->mocap_meas[1] = state->p_buffer[read_index][1] + generate_gaussian_noise(param->sigma_pos, param->f_MoCap);
+    state->mocap_meas[2] = state->p_buffer[read_index][2] + generate_gaussian_noise(param->sigma_pos, param->f_MoCap);
 
     state->write_index = (state->write_index + 1) % 2;
 }
