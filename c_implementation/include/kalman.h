b84079dfe4cb6887944df02065ca65c05065a2a2
thomasduroy <thomas.duroy72@gmail.com>
2026-06-02 08:38:01 +0200

    refactor kalman

--- c_implementation/include/kalman.h
+++ c_implementation/include/kalman.h
@@ -7,10 +7,11 @@ typedef struct {
     double U_kalman[4];
     double X_hat[15];
     double A_u;
-    double B_u; 
+    double B_u;
 } KalmanState;
 
 void kalman_init(KalmanState* state, Param* param);
-void kalman_update(double* u, double* Y_meas, double* acc_meas, double* X_hat, KalmanState* state, Param* param);
+void kalman_outputs(KalmanState* state, double* X_hat);
+void kalman_update(double* u, double* Y_meas, double* acc_meas, KalmanState* state, Param* param);
 
 #endif // KALMAN_H
--- c_implementation/src/kalman.c
+++ c_implementation/src/kalman.c
@@ -2,10 +2,9 @@
 #include <math.h>
 
 void kalman_init(KalmanState* state, Param* param) {
-    state->U_kalman[0] = param->m_tilde * param->g;
-    state->U_kalman[1] = 0.0;
-    state->U_kalman[2] = 0.0;
-    state->U_kalman[3] = 0.0;
+    for (int i = 0; i < 4; i++) {
+        state->U_kalman[i] = 0.0;
+    }
 
     for (int i = 0; i < 15; i++) {
         state->X_hat[i] = 0.0;
@@ -15,30 +14,25 @@ void kalman_init(KalmanState* state, Param* param) {
     state->B_u = 1.0 - state->A_u;
 }
 
-void kalman_update(double* u, double* Y_meas, double* acc_meas, double* X_hat, KalmanState* state, Param* param) {
-    int i, j;
-
-    for (i = 0; i < 15; i++) {
+void kalman_outputs(KalmanState* state, double* X_hat) {
+    for (int i = 0; i < 15; i++) {
         X_hat[i] = state->X_hat[i];
     }
+}
 
-    double u_adapted[4];
-    u_adapted[0] = u[0] - (param->m_tilde * param->g);
-    u_adapted[1] = u[1];
-    u_adapted[2] = u[2];
-    u_adapted[3] = u[3];
-
+void kalman_update(double* u, double* Y_meas, double* acc_meas, KalmanState* state, Param* param) {
     double acc_kalman[3];
     acc_kalman[0] = acc_meas[0];
     acc_kalman[1] = acc_meas[1];
     acc_kalman[2] = acc_meas[2] - param->g;
 
+
     double Y[12];
-    for (i = 0; i < 9; i++)  Y[i] = Y_meas[i];
-    for (i = 0; i < 3; i++)  Y[i + 9] = acc_kalman[i];
+    for (int i = 0; i < 9; i++)  Y[i] = Y_meas[i];
+    for (int i = 0; i < 3; i++)  Y[i + 9] = acc_kalman[i];
 
     double Y_hat[12] = {0.0};
-    for (i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++) {
         Y_hat[i] = state->X_hat[i];
         Y_hat[i+3] = state->X_hat[i+3];
         Y_hat[i+6] = state->X_hat[i+9];
@@ -48,28 +42,34 @@ void kalman_update(double* u, double* Y_meas, double* acc_meas, double* X_hat, K
     Y_hat[11] = state->X_hat[14];
 
     double innovation[12];
-    for (i = 0; i < 12; i++) {
+    for (int i = 0; i < 12; i++) {
         innovation[i] = Y[i] - Y_hat[i];
     }
 
     double x_next[15] = {0.0};
-    for (i = 0; i < 15; i++) {
-        for (j = 0; j < 15; j++) {
+    for (int i = 0; i < 15; i++) {
+        for (int j = 0; j < 15; j++) {
             x_next[i] += param->A_kalman[i][j] * state->X_hat[j];
         }
-        for (j = 0; j < 4; j++) {
+        for (int j = 0; j < 4; j++) {
             x_next[i] += param->B_kalman[i][j] * state->U_kalman[j];
         }
-        for (j = 0; j < 12; j++) {
+        for (int j = 0; j < 12; j++) {
             x_next[i] += param->K_kalman[i][j] * innovation[j];
         }
     }
 
-    for (i = 0; i < 4; i++) {
-        state->U_kalman[i] = state->A_u * state->U_kalman[i] + state->B_u * u_adapted[i];
+    for (int i = 0; i < 15; i++) {
+        state->X_hat[i] = x_next[i];
     }
 
-    for (i = 0; i < 15; i++) {
-        state->X_hat[i] = x_next[i];
+    double u_adapted[4];
+    u_adapted[0] = u[0] - (param->m_tilde * param->g);
+    u_adapted[1] = u[1];
+    u_adapted[2] = u[2];
+    u_adapted[3] = u[3];
+
+    for (int i = 0; i < 4; i++) {
+        state->U_kalman[i] = state->A_u * state->U_kalman[i] + state->B_u * u_adapted[i];
     }
 }
