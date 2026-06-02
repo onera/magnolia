f5aa9991ba96bd429fa8dc5f2fb376a4b463aa3a
thomasduroy <thomas.duroy72@gmail.com>
2026-06-02 08:51:55 +0200

    refactor lqi

--- c_implementation/src/lqi.c
+++ c_implementation/src/lqi.c
@@ -4,28 +4,19 @@ void lqi_init(LQIState* state) {
     for (int i = 0; i < 3; i++) {
         state->epsilon[i] = 0.0;
     }
+    for (int i = 0; i < 6; i++) {
+        state->prev_state[i] = 0.0;
+    }
 }
 
-void lqi_update(double* X, double* eta_ref, double* tau, LQIState* state, Param* param) {
-
-    double dt = 1.0 / param->f_lqi;
-    double error_now[3];
-
-    error_now[0] = eta_ref[0] - X[3]; 
-    error_now[1] = eta_ref[1] - X[4]; 
-    error_now[2] = eta_ref[2] - X[5]; 
-
-    for (int i = 0; i < 3; i++) {
-        state->epsilon[i] += dt * error_now[i];
-    }
-
+void lqi_outputs(double* tau, LQIState* state, Param* param) {
     double X_aug[9];
-    X_aug[0] = X[3];   
-    X_aug[1] = X[4];   
-    X_aug[2] = X[5]; 
-    X_aug[3] = X[9];   
-    X_aug[4] = X[10];  
-    X_aug[5] = X[11];  
+    X_aug[0] = state->prev_state[0];   
+    X_aug[1] = state->prev_state[1];   
+    X_aug[2] = state->prev_state[2]; 
+    X_aug[3] = state->prev_state[3];   
+    X_aug[4] = state->prev_state[4];  
+    X_aug[5] = state->prev_state[5];  
     X_aug[6] = state->epsilon[0];
     X_aug[7] = state->epsilon[1];
     X_aug[8] = state->epsilon[2];
@@ -35,5 +26,24 @@ void lqi_update(double* X, double* eta_ref, double* tau, LQIState* state, Param*
         for (int j = 0; j < 9; j++) {
             tau[i] -= param->K_lqi[i][j] * X_aug[j];
         }
+    }
+}
+
+void lqi_update(double* X, double* eta_ref, LQIState* state, Param* param) {
+
+    double dt = 1.0 / param->f_lqi;
+    double error[3];
+
+    error[0] = eta_ref[0] - X[3]; 
+    error[1] = eta_ref[1] - X[4]; 
+    error[2] = eta_ref[2] - X[5]; 
+
+    for (int i = 0; i < 3; i++) {
+        state->epsilon[i] += dt * error[i];
+    }
+
+    for (int i = 0; i < 3; i++) {
+        state->prev_state[i] = X[i+3];
+        state->prev_state[i+3] = X[i+9];
     }
 }
