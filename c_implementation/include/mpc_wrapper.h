487bc0cb9b7e47e298df50c327c62151d1ca1a5e
thomasduroy <thomas.duroy72@gmail.com>
2026-06-02 09:52:04 +0200

    refactor mpc_wrapper

--- c_implementation/include/mpc_wrapper.h
+++ c_implementation/include/mpc_wrapper.h
@@ -2,19 +2,22 @@
 #define MPC_WRAPPER_H
 
 #include "param.h"
+#include "gains.h"
 #include "../lib/osqp_c_code/include/osqp.h" 
 
 typedef struct {
     double epsilon[3];
+    double u_prev[4];
     
-    c_float q_new[129]; 
-    c_float l_new[639];   
-    c_float u_new[639];   
+    c_float q_new[PARAM_NX * (PARAM_NP + 1) + PARAM_NU * PARAM_NC]; 
+    c_float l_new[2*PARAM_NX*(PARAM_NP + 1) + PARAM_NU * PARAM_NC];   
+    c_float u_new[2*PARAM_NX*(PARAM_NP + 1) + PARAM_NU * PARAM_NC];   
 } MPCState;
 
 void mpc_wrapper_init(MPCState* state);
 
-void mpc_wrapper_step(double* X_est, double* X_ref_horizon, double* U_optimal_out, 
-                      MPCState* state, Param* param);
+void mpc_wrapper_outputs(double* u, MPCState* state);
+
+void mpc_wrapper_update(double* X_hat, double* X_ref, MPCState* state, Param* param);
 
 #endif // MPC_WRAPPER_H
--- c_implementation/src/mpc_wrapper.c
+++ c_implementation/src/mpc_wrapper.c
@@ -7,32 +7,32 @@ extern OSQPWorkspace workspace;
 
 void mpc_wrapper_init(MPCState* state) {
     memset(state->epsilon, 0, sizeof(state->epsilon));
+    memset(state->u_prev, 0, sizeof(state->u_prev));
     memset(state->q_new, 0, sizeof(state->q_new));
     memset(state->l_new, 0, sizeof(state->l_new));
     memset(state->u_new, 0, sizeof(state->u_new));
 }
 
-void mpc_wrapper_step(double* X_est, double* X_ref_horizon, double* U_optimal_out, 
-                      MPCState* state, Param* param) {
-    
+void mpc_wrapper_outputs(double* u, MPCState* state) {
+    for (int i = 0; i < 4; i++) {
+        u[i] = state->u_prev[i];
+    }
+}
+
+void mpc_wrapper_update(double* X_hat, double* X_ref, MPCState* state, Param* param) {
     int n = param->nx;       
     int m = param->nu;     
     int Np = param->Np;    
     int Nc = param->Nc;     
     int stride = Np + 1;    
 
-    double Ts_mpc = 1.0 / param->f_mpc;
-    state->epsilon[0] += Ts_mpc * (X_ref_horizon[0 * stride] - X_est[0]); 
-    state->epsilon[1] += Ts_mpc * (X_ref_horizon[1 * stride] - X_est[1]); 
-    state->epsilon[2] += Ts_mpc * (X_ref_horizon[2 * stride] - X_est[2]); 
-
     c_float X_aug[9];
-    X_aug[0] = (c_float)X_est[0]; 
-    X_aug[1] = (c_float)X_est[1];
-    X_aug[2] = (c_float)X_est[2]; 
-    X_aug[3] = (c_float)X_est[3]; 
-    X_aug[4] = (c_float)X_est[4];
-    X_aug[5] = (c_float)X_est[5]; 
+    X_aug[0] = (c_float)X_hat[0]; 
+    X_aug[1] = (c_float)X_hat[1];
+    X_aug[2] = (c_float)X_hat[2]; 
+    X_aug[3] = (c_float)X_hat[3]; 
+    X_aug[4] = (c_float)X_hat[4];
+    X_aug[5] = (c_float)X_hat[5]; 
     X_aug[6] = (c_float)state->epsilon[0]; 
     X_aug[7] = (c_float)state->epsilon[1]; 
     X_aug[8] = (c_float)state->epsilon[2]; 
@@ -40,9 +40,9 @@ void mpc_wrapper_step(double* X_est, double* X_ref_horizon, double* U_optimal_ou
     int idx_q = 0;
 
     for (int j = 0; j < Np; j++) {
-        double rx = X_ref_horizon[0 * stride + j];
-        double ry = X_ref_horizon[1 * stride + j];
-        double rz = X_ref_horizon[2 * stride + j];
+        double rx = X_ref[0 * stride + j];
+        double ry = X_ref[1 * stride + j];
+        double rz = X_ref[2 * stride + j];
 
         state->q_new[idx_q++] = (c_float)(-param->Q_mpc[0] * rx); 
         state->q_new[idx_q++] = (c_float)(-param->Q_mpc[1] * ry); 
@@ -56,16 +56,16 @@ void mpc_wrapper_step(double* X_est, double* X_ref_horizon, double* U_optimal_ou
     }
 
     double X_ref_terminal[9] = {0.0};
-    X_ref_terminal[0] = X_ref_horizon[0 * stride + Np]; 
-    X_ref_terminal[1] = X_ref_horizon[1 * stride + Np]; 
-    X_ref_terminal[2] = X_ref_horizon[2 * stride + Np]; 
+    X_ref_terminal[0] = X_ref[0 * stride + Np]; 
+    X_ref_terminal[1] = X_ref[1 * stride + Np]; 
+    X_ref_terminal[2] = X_ref[2 * stride + Np]; 
 
     for (int i = 0; i < n; i++) {
-        double double_sum = 0.0;
+        double sum = 0.0;
         for (int k = 0; k < n; k++) {
-            double_sum += param->Q_Terminal[i][k] * X_ref_terminal[k];
+            sum += param->Q_Terminal[i][k] * X_ref_terminal[k];
         }
-        state->q_new[idx_q++] = (c_float)(-double_sum);
+        state->q_new[idx_q++] = (c_float)(-sum);
     }
 
     for (int i = 0; i < (m * Nc); i++) {
@@ -108,6 +108,11 @@ void mpc_wrapper_step(double* X_est, double* X_ref_horizon, double* U_optimal_ou
 
     int cmd_start_idx = n * (Np + 1);
     for (int i = 0; i < m; i++) {
-        U_optimal_out[i] = (double)workspace.solution->x[cmd_start_idx + i];
+        state->u_prev[i] = (double)workspace.solution->x[cmd_start_idx + i];
     }
+
+    double Ts_mpc = 1.0 / param->f_mpc;
+    state->epsilon[0] += Ts_mpc * (X_ref[0 * stride] - X_hat[0]); 
+    state->epsilon[1] += Ts_mpc * (X_ref[1 * stride] - X_hat[1]); 
+    state->epsilon[2] += Ts_mpc * (X_ref[2 * stride] - X_hat[2]); 
 }
